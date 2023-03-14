bc236bb7d57010728783e53f7f2e39bbb6f7e8d4
EwuraAmaSam <ewura.sam@ashesi.edu.gh>
2023-03-14 21:24:27 +0000

    BAd word filter

---
+++ Bad.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Bad {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+	    String sentence;
+		String[] bad_words = {"fuck", "daug", "nigga", "motherfucker", "bitch"};
+        System.out.print("Type a sentence: ");
+		sentence = scan.nextLine();
+
+        int num_bad = 0;
+        for (int i = 0; i < bad_words.length; i++){
+            if (sentence.toLowerCase().contains(bad_words[i].toLowerCase())){
+                num_bad++;
+            }
+        }
+        if (num_bad != 0){
+            System.out.println("There are " + num_bad + " bad words in the sentence");
+        }
+        else{
+            System.out.println("There are no bad words.");
+        }
+    
+    
+    }
+}
